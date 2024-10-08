a94f81ae85b71e9756aebf4bab4a4c3c4171765f
phoebe-lavan <phoebe.lavan@gmail.com>
2024-10-08 22:48:39 +1100

    Correct and Inforect answer styling

--- src/components/ui/quiz-card-result.tsx
+++ src/components/ui/quiz-card-result.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { useState } from "react";
+import { CorrectIcon, IncorrectIcon } from "./icons";
 
 interface QuizCardResultProps {
   questionNumber: number;
@@ -20,7 +21,7 @@ export default function QuizCardResult({
   const points = 0.5; // Default points for each question
 
   return (
-    <Card className="mb-10 mx-auto max-w-lg">
+    <Card className="mb-10 mx-auto max-w-lg"> 
       <CardHeader className="flex justify-between bg-secondary p-4 rounded-sm">
         <CardTitle className="text-xl">Questionnnnnn {questionNumber}</CardTitle>
         <div className="font-semibold">{points} Points</div>
@@ -32,12 +33,14 @@ export default function QuizCardResult({
             <ul className="flex items-center gap-2 hover:text-primary">
               
               <li
-                // type="radio"
-                //name={`question-${questionNumber}`}
                 value={option}
-              //  checked={selectedOption === option} 
               />
-              <span>{option}</span>
+              {/* TODO uncomment and update variable name to show correct and incorrect answers */}
+              {/* <div className={validation == false ?"text-red-600" : validation == true ? "text-lime-600" : ""}> */}
+                <span >{option}</span>
+                {/* { validation == false ? <IncorrectIcon/> : validation == true ? <CorrectIcon/> : ""} */}
+              {/* </div> */}
+              
             </ul>
             <Separator className="mt-3" />
           </div>
