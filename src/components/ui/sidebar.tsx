cbcd77fa7a3b5d84c9387845ac573f123cf7745d
bhav-18 <sohanakaran@gmail.com>
2024-10-16 10:23:24 +1100

    update analytics tab to redirect to results page

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -23,7 +23,7 @@ export default function Sidebar() {
               Dashboard
             </Link>
             <Link
-              href="http://localhost:3000/quiz"
+              href="http://localhost:3000/results"
               className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary mb-4"
             >
               <LineChart className="h-4 w-4" />
