4c83b840c117669fa7dd1d8c408c026d1281c684
deep.singh <deepsohana18@gmail.com>
2024-10-23 10:26:43 +1100

    create user schema

---
+++ src/models/user-model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        unique: false,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        unique: false,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: false,
+    },
+});
+
+const User = mongoose.models.users || mongoose.model('users', userSchema);
+
+export default User;
