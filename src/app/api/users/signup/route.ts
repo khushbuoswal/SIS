0df5c6ea13f75dec44f446601f72ddbe6defdebc
deep.singh <deepsohana18@gmail.com>
2024-10-23 10:25:26 +1100

    POST functionality for register

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,48 @@
+import { connectMongoDB } from "@/db-config/db-config";
+import User from '@/models/user-model';
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from 'bcryptjs';
+
+connectMongoDB();
+
+// POST route to create new user in DB
+export async function POST(request: NextRequest) {
+    try {
+        const requestBody = await request.json();
+        const { firstName, lastName, email, password } = requestBody;
+        // REMOVE FROM PROD
+        console.log(requestBody)
+
+        const user = await User.findOne({ email });
+        if (user) {
+            return NextResponse.json({
+                error: 'This user already exists.'
+            },
+                { status: 400 }
+            );
+        }
+
+        // Hash the password
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+
+        // Create new user
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword
+        });
+
+        // Save the new user in DB
+        const savedUser = await newUser.save();
+
+        return NextResponse.json({
+            message:'User created successfully.',
+            success: true,
+            savedUser,
+        });
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
